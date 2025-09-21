51e8ffe057504d55c480d4d1cd3a8e9d171e3ba9
Arjun <techgeekarjun18@gmail.com>
2025-09-21 10:02:42 +0530

    commit all

---
+++ src/main/java/com/websocket/appConfig/Controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.websocket.appConfig.Controller;
+
+
+import com.websocket.appConfig.Model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+
+}
---
+++ src/main/java/com/websocket/appConfig/Model/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.websocket.appConfig.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+
+    private Long id;
+    private String sender;
+    private String content;
+}
